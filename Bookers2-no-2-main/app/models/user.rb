745beab89d922959469cd3dee5683b510ca0bb8e
asatokouki <okometabero@me.com>
2021-11-04 07:03:51 +0000

    [Fix]いいね追加できて、削除できるようにした。あとはインデックスに数表示するだけ

--- Bookers2-no-2-main/app/models/user.rb
+++ Bookers2-no-2-main/app/models/user.rb
@@ -4,8 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    has_many :book_comments, dependent: :destroy
+
     has_many :books, dependent: :destroy
+    has_many :book_comments, dependent: :destroy
+    has_many :favorites, dependent: :destroy
+
     attachment :profile_image
 
   validates :name, presence: true, length: { in: 2..20 }, uniqueness: true
